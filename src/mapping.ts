6f37b77f80eacceeca8cad8c5dbcb6c1b1e05221
Ford Nickels <ford@thegraph.com>
2019-03-29 18:12:21 -0700

    Setup SportX subgraph

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,22 +1,32 @@
-import { NewGravatar, UpdatedGravatar } from './types/Gravity/Gravity'
-import { Gravatar } from './types/schema'
+import { Market, Order } from './types/schema'
+import { MarketRegistered, MarketUnregistered } from './types/Market/MarketRegistry'
+import { OrderFill } from "./types/FillOrder/FillOrder";
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+export function handleMarketRegistered(event: MarketRegistered): void {
+  let market = new Market((event.params.marketHash.toHex()))
+
+  market.title = event.params.marketRequest.get('title').toString()
+  market.referenceUrl = event.params.marketRequest.get('referenceUrl').toString()
+  market.outcomeOne = event.params.marketRequest.get('outcomeOneName').toString()
+  market.outcomeTwo = event.params.marketRequest.get('outcomeTwoName').toString()
+  market.outcomeVoid = event.params.marketRequest.get('outcomeVoidName').toString()
+  market.registered = true
+  market.save()
 }
 
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
-  }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+export function handleMarketUnRegistered(event: MarketUnregistered): void {
+  let market = Market.load( event.params.marketHash.toHex())
+  market.registered = false
+  market.save()
+}
+
+export function handleOrder(event: OrderFill): void {
+  let order = new Order(event.params.orderHash.toHex())
+
+  order.maker= event.params.maker
+  order.taker= event.params.taker
+  order.newFilledAmount = event.params.newFilledAmount
+  order.totalBetSize = event.params.makerOrder.totalBetSize
+  order.potSize = event.params.orderAmounts.potSize
+  order.save()
 }
