38cc558db3671e15d822117a07b82ff8e342b35d
Ford Nickels <ford@thegraph.com>
2019-04-04 15:00:30 -0700

    Store all event params from market and fill order events

--- src/mapping.ts
+++ src/mapping.ts
@@ -5,11 +5,12 @@ import { OrderFill } from "./types/FillOrder/FillOrder";
 export function handleMarketRegistered(event: MarketRegistered): void {
   let market = new Market((event.params.marketHash.toHex()))
 
-  market.title = event.params.marketRequest.get('title').toString()
-  market.referenceUrl = event.params.marketRequest.get('referenceUrl').toString()
-  market.outcomeOne = event.params.marketRequest.get('outcomeOneName').toString()
-  market.outcomeTwo = event.params.marketRequest.get('outcomeTwoName').toString()
-  market.outcomeVoid = event.params.marketRequest.get('outcomeVoidName').toString()
+  market.title = event.params.marketRequest.title
+  market.baseToken = event.params.marketRequest.baseToken
+  market.referenceUrl = event.params.marketRequest.referenceUrl
+  market.outcomeOne = event.params.marketRequest.outcomeOneName
+  market.outcomeTwo = event.params.marketRequest.outcomeTwoName
+  market.outcomeVoid = event.params.marketRequest.outcomeVoidName
   market.registered = true
   market.save()
 }
@@ -27,6 +28,19 @@ export function handleOrder(event: OrderFill): void {
   order.taker= event.params.taker
   order.newFilledAmount = event.params.newFilledAmount
   order.totalBetSize = event.params.makerOrder.totalBetSize
+  order.percentageOdds = event.params.makerOrder.percentageOdds
+  order.expiry = event.params.makerOrder.expiry
+  order.oracleFee = event.params.makerOrder.oracleFee
+  order.relayerMakerFee = event.params.makerOrder.relayerMakerFee
+  order.relayerTakerFee = event.params.makerOrder.relayerTakerFee
+  order.salt= event.params.makerOrder.salt
+  order.isMakerBettingOutcomeOne = event.params.makerOrder.isMakerBettingOutcomeOne
+
+  order.takerAmount = event.params.orderAmounts.takerAmount
+  order.takerEscrow = event.params.orderAmounts.takerEscrow
   order.potSize = event.params.orderAmounts.potSize
+  order.makerOracleFee = event.params.orderAmounts.makerOracleFee
+  order.takerOracleFee = event.params.orderAmounts.takerOracleFee
+
   order.save()
 }
